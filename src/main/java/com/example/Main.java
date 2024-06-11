b5670aa2c80ba0c7a93609533c44d51f9c795acd
Roberto Méndez <roberto.mendez@lastminute.com>
2024-06-11 21:06:22 +0200

    Initial commit

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+            System.out.println("Crappy formatting");
+
+        String nullText = null;
+        if (nullText.equals("example")) {
+            System.out.println("This breaks");
+        }
+    }
+}
---
+++ src/test/java/com/example/ExampleTest.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import java.io.File;
+import java.io.IOException;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class ExampleTest {
+
+    @Test
+    public void simpleTest() {
+        Assert.assertEquals(42, 42);
+    }
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void temporaryFileTest() throws IOException {
+        File testFile = folder.newFile("test.txt");
+        Assert.assertTrue(testFile.exists());
+    }
+
+    @Test(expected = IOException.class)
+    public void expectedExceptionTest() throws IOException {
+        maybeThrowException(true);
+    }
+
+    private void maybeThrowException(boolean maybe) throws IOException {
+        if (maybe) {
+            throw new IOException();
+        }
+    }
+}
